2a3e0ded640983cfc0c12d238055fa434bcf12e0
Kevin Li <lishuai19890928@gmail.com>
2023-12-04 00:00:25 -0800

    Adding NavBar #1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
 import store from './store/store.js'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
